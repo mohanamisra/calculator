a4107607ad870c8a7d4d726d41a250f348c0a320
Mohana Misra <iammohanamisra@gmail.com>
2022-12-17 11:05:52 +0530

    Operation functionality started

--- calculator.js
+++ calculator.js
@@ -5,9 +5,15 @@ let buttons = document.getElementsByTagName("input");
 let clearButton = document.getElementById("clear-button");
 let equalsButton = document.getElementById("equals-button");
 let percentageButton = document.getElementsByClassName("percentage-button")[0];
+let signButton = document.getElementsByClassName("sign-button")[0];
 
 let arrDisplay = [];
 let arrDisplayString;
+let finalArrDisplay = [];
+let finalArrDisplayString;
+
+let lastElement;
+let secondLastElement;
 
 for(let button of buttons){
     button.addEventListener("click", ()=>{
@@ -15,13 +21,15 @@ for(let button of buttons){
         if(button.classList.contains("simple")){
             arrDisplay.push(button.value);
             
-            let lastElement = arrDisplay[arrDisplay.length - 1];
-            let secondLastElement = arrDisplay[arrDisplay.length - 2];
+            lastElement = arrDisplay[arrDisplay.length - 1];
+            secondLastElement = arrDisplay[arrDisplay.length - 2];
             if(((secondLastElement == '/' || secondLastElement == '*' || secondLastElement == '-' || secondLastElement == '+' || secondLastElement == '.') && (lastElement == '/' || lastElement == '*' || lastElement == '-' || lastElement == '+' || lastElement == '.')) || (arrDisplay.length > 10))
                 arrDisplay.pop();
 
             arrDisplayString = arrDisplay.join("");
             displayVal.innerHTML = arrDisplayString;
+
+            finalArrDisplay = arrDisplayString.split(/[+-/*]/);
         }
         if(button === clearButton){
             displayVal.innerHTML = "0";
@@ -32,6 +40,9 @@ for(let button of buttons){
             arrDisplay.push(displayVal.innerHTML/100);
             arrDisplayString = arrDisplay.join("");
             displayVal.innerHTML = arrDisplayString;
+        }
+        if(button === equalsButton){
+            console.log(finalArrDisplay);
         }
     });
 }
